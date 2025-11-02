9af265cec39f3b4191071dcffc39466a70282e29
JJCAPPE <bujack@bu.edu>
2025-11-02 17:32:44 -0500

    buzzer test

--- buzzer_test.py
+++ buzzer_test.py
@@ -2,5 +2,6 @@
 from hw.buzzer import Buzzer
 
 buzzer = Buzzer()
-buzzer.beep_pattern(count=3, duration=0.2, pause=0.1)
+buzzer.beep_pattern(count=10, duration=0.2, pause=0.1)
+buzzer.beep_pattern(count=10, duration=1.0, pause=0.1)
 buzzer.cleanup()
